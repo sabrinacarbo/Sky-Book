6c5d484fb25f3534de57a991d4c0be5f2b6a8208
Sabrina Carbo <sabrinacarbo@Sabrinas-MacBook-Pro.local>
2018-08-03 13:49:00 -0400

    fixes linting errors

--- models/user.js
+++ models/user.js
@@ -2,52 +2,51 @@ module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     name: {
       type: DataTypes.STRING,
-      validate:{
-        notEmpty:{
-        msg:"Please enter a user name"
-      }
-    },
-
-    password: {
-      type: Datatype.STRING,
-      allowNull: false,
-      validate:{
-        isAlphanumeric: {
-          msg:"Password only allows numbers and letters"
+      validate: {
+        notEmpty: {
+          msg: "Please enter a user name"
         }
-      }
-    },
+      },
 
-    img: {
-      type:Datatypr.STRING,
-      allowNull:false
-    },
-
-    license:{
-      type:Datatype.STRING,
-      allowNull: true
-    },
-
-    numberOfJump:{
-      type: Datatype.INTEGER,
-      validate:{
-        isInt:{
-          msg:"Number of Jumps must be an integer"
+      password: {
+        type: Datatype.STRING,
+        allowNull: false,
+        validate: {
+          isAlphanumeric: {
+            msg: "Password only allows numbers and letters"
+          }
         }
+      },
+
+      img: {
+        type: Datatypr.STRING,
+        allowNull: false
+      },
+
+      license: {
+        type: Datatype.STRING,
+        allowNull: true
+      },
+
+      numberOfJump: {
+        type: Datatype.INTEGER,
+        validate: {
+          isInt: {
+            msg: "Number of Jumps must be an integer"
+          }
+        }
+      },
+
+      homeDropZone: {
+        type: Datatype.STRING,
+        allowNull: true
+      },
+
+      bio: {
+        type: Datatype.STRING,
+        allowNull: true
       }
-    },
-
-    homeDropZone:{
-      type: Datatype.STRING,
-      allowNull: true
-    },
-
-    bio:{
-      type:Datatype.STRING,
-      allowNull: true
     }
-  }
-    
   });
 
   User.associate = function(models) {
