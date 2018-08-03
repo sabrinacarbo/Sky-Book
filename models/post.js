597b1a903ca8af39d4c3718f998815419cd5aaf0
Ruohan Zhang <ruohan.zhang@outlook.com>
2018-08-03 13:28:14 -0400

    add models

---
+++ models/post.js
@@ -0,0 +1,32 @@
+module.exports = function(sequelize, DataTypes) {
+    var Post = sequelize.define("Post", {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [0,500]
+        }
+      },
+      body: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: sequelize.literal("CURRENT_TIMESTAMP")
+      }
+     
+    });
+  
+    Post.associate = function(models) {
+      Post.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
+    return Post;
+  };
---
+++ models/user.js
@@ -0,0 +1,60 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    name: {
+      type: DataTypes.STRING,
+      validate:{
+        notEmpty:{
+        msg:"Please enter a user name"
+      }
+    },
+
+    password: {
+      type: Datatype.STRING,
+      allowNull: false,
+      validate:{
+        isAlphanumeric: {
+          msg:"Password only allows numbers and letters"
+        }
+      }
+    },
+
+    img: {
+      type:Datatypr.STRING,
+      allowNull:false
+    },
+
+    license:{
+      type:Datatype.STRING,
+      allowNull: true
+    },
+
+    numberOfJump:{
+      type: Datatype.INTEGER,
+      validate:{
+        isInt:{
+          msg:"Number of Jumps must be an integer"
+        }
+      }
+    },
+
+    homeDropZone:{
+      type: Datatype.STRING,
+      allowNull: true
+    },
+
+    bio:{
+      type:Datatype.STRING,
+      allowNull: true
+    }
+  }
+    
+  });
+
+  User.associate = function(models) {
+    User.hasMany(models.Post, {
+      onDelete: "cascade"
+    });
+  };
+
+  return User;
+};
