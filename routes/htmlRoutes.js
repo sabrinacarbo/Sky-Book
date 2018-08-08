d2b24c85a8af1021e0b9013a1cf95664ba85a44f
hankzimmer7 <hankzimmer7@gmail.com>
2018-08-08 15:46:41 -0400

    fixes linting

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,9 +4,9 @@ var express = require("express");
 var router = express.Router();
 
 // Load index page
-router.get("/", function (req, res) {
-  db.DZ.findAll().then(function (DZs) {
-    db.Boogie.findAll().then(function (boogies) {
+router.get("/", function(req, res) {
+  db.DZ.findAll().then(function(DZs) {
+    db.Boogie.findAll().then(function(boogies) {
       res.render("index", {
         DZs: DZs,
         boogies: boogies
@@ -16,22 +16,22 @@ router.get("/", function (req, res) {
 });
 
 // Load posts page
-router.get("/posts", function (req, res) {
-  db.Post.findAll().then(function (allPosts) {
+router.get("/posts", function(req, res) {
+  db.Post.findAll().then(function(allPosts) {
     res.render("posts", {
       allPosts: allPosts
     });
   });
 });
 
-router.get("/profile/:id", function (req, res) {
+router.get("/profile/:id", function(req, res) {
   console.log("/profile/" + req.params.id);
-  db.DZ.findAll().then(function (DZs) {
+  db.DZ.findAll().then(function(DZs) {
     db.User.findOne({
       where: {
         id: req.params.id
       }
-    }).then(function (dbUser) {
+    }).then(function(dbUser) {
       res.render("profile-info", {
         name: dbUser.name,
         img: dbUser.img,
@@ -46,7 +46,7 @@ router.get("/profile/:id", function (req, res) {
 });
 
 // Render 404 page for any unmatched routes
-router.get("*", function (req, res) {
+router.get("*", function(req, res) {
   res.render("404");
 });
 
