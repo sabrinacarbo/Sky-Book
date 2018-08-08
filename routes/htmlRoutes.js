c62a08e5248d5bc34988548e9943070dbeab1961
hankzimmer7 <hankzimmer7@gmail.com>
2018-08-08 12:58:04 -0400

    adds a new posts page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,30 +4,26 @@ var express = require("express");
 var router = express.Router();
 
 // Load index page
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.render("index");
 });
 
-// Load profile page and pass in a user by id
-// Need to modify this to query to database for the info, and use a variable in the url for the username
-// app.get("/profile/hank", function(req, res) {
-//   res.render("profile-info", {
-//     name: "Hank",
-//     img: "https://hankzimmer7.github.io/assets/images/profile_picture.jpg",
-//     license: "none",
-//     numberOfJump: 0,
-//     homeDropZone: "ATL",
-//     bio: "I love the sky, but I can't jump"
-//   });
-// });
+// Load posts page
+router.get("/posts", function (req, res) {
+  db.Post.findAll().then(function (allPosts) {
+    console.log(allPosts);
+    console.log()
+    res.render("posts", {allPosts});
+  });
+});
 
-router.get("/profile/:id", function(req, res) {
+router.get("/profile/:id", function (req, res) {
   console.log("/profile/" + req.params.id);
   db.User.findOne({
     where: {
       id: req.params.id
     }
-  }).then(function(dbUser) {
+  }).then(function (dbUser) {
     res.render("profile-info", {
       name: dbUser.name,
       img: dbUser.img,
@@ -40,7 +36,7 @@ router.get("/profile/:id", function(req, res) {
 });
 
 // Render 404 page for any unmatched routes
-router.get("*", function(req, res) {
+router.get("*", function (req, res) {
   res.render("404");
 });
 
